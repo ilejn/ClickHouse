2c9544994a1284625e28ec2d8b53d7b0314acc43
Ilya Golshtein <igolshtein@altinity.com>
2023-04-29 21:19:11 +0000

    simplified_partition_mutations: some debug

--- src/Storages/StorageMergeTree.cpp
+++ src/Storages/StorageMergeTree.cpp
@@ -927,7 +927,7 @@ MergeMutateSelectedEntryPtr StorageMergeTree::selectPartsToMerge(
         if (getCurrentMutationVersion(left, lock) != getCurrentMutationVersion(right, lock))
         {
             if (disable_reason)
-                *disable_reason = "Some parts have differ mmutatuon version";
+                *disable_reason = "Some parts have different mutation versions";
             return false;
         }
 
@@ -1378,8 +1378,14 @@ UInt64 StorageMergeTree::getCurrentMutationVersion(
 {
     auto it = current_mutations_by_version.upper_bound(part->info.getDataVersion());
     if (it == current_mutations_by_version.begin())
+    {
+
+        LOG_TRACE(log, "getCurrentMutationVersion: {}", 0);
         return 0;
+    }
+
     --it;
+    LOG_TRACE(log, "getCurrentMutationVersion: {}", it->first);
     return it->first;
 }
 
