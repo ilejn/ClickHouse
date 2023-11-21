c665d55c40889b8344a4ef1a9e608c059456cd32
Mike Kot <myrrc@yandex-team.ru>
2023-11-21 14:24:20 +0000

    boilerplate code for locks

--- src/Disks/ObjectStorages/DiskObjectStorageTransaction.h
+++ src/Disks/ObjectStorages/DiskObjectStorageTransaction.h
@@ -147,8 +147,5 @@ public:
     void chmod(const String & path, mode_t mode) override;
     void setReadOnly(const std::string & path) override;
     void createHardLink(const std::string & src_path, const std::string & dst_path) override;
-
-    bool lock(std::string_view path, bool block) override;
-    void unlock(std::string_view path) override;
 };
 }
