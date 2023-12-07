0811b6ec2c0cb2460ff84b7b035cb133bf9a22aa
Ilya Golshtein <igolshtein@altinity.com>
2023-12-07 15:27:06 +0300

    disk-object-storage-vfs: tiny cleanup

--- programs/server/Server.cpp
+++ programs/server/Server.cpp
@@ -659,7 +659,6 @@ try
     registerRemoteFileMetadatas();
     registerSchedulerNodes();
     registerResourceManagers();
-//    registerDisks(/* global_skip_access_check= */ false, /* allow_vfs */ true);
 
     CurrentMetrics::set(CurrentMetrics::Revision, ClickHouseRevision::getVersionRevision());
     CurrentMetrics::set(CurrentMetrics::VersionInteger, ClickHouseRevision::getVersionInteger());
@@ -1492,7 +1491,6 @@ try
 #endif
                 });
         }
-        // registerDisks(/* global_skip_access_check= */ false, /* allow_vfs */ true);
 #else
         throw Exception(ErrorCodes::SUPPORT_IS_DISABLED, "ClickHouse server built without NuRaft library. Cannot use internal coordination.");
 #endif
